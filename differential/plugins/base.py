50c63b06729f189062453a5065651a7243174108
Lei Shi <me@leishi.io>
2021-10-13 16:07:39 +0200

    move most of the torrentInfo to Base class

--- differential/plugins/base.py
+++ differential/plugins/base.py
@@ -426,11 +426,280 @@ class Base(ABC, metaclass=PluginRegister):
         if self.make_torrent:
             self._mktorrent()
 
-    @abstractmethod
-    def upload(self):
-        raise NotImplementedError()
+    @property
+    def title(self):
+        # TODO: Either use file name or generate from mediainfo and ptgen
+        temp_name = Path(self.folder).stem.replace('.', ' ')
+        temp_name = temp_name.replace(' 5 1 ', ' 5.1 ')
+        temp_name = temp_name.replace(' 7 1 ', ' 7.1 ')
+        return temp_name
 
     @property
-    @abstractmethod
+    def subtitle(self):
+        if not self._ptgen.get("site") == "douban":
+            return ""
+        subtitle = f"{'/'.join(self._ptgen.get('this_title', []) + self._ptgen.get('aka', []))}"
+        if self._ptgen.get("director"):
+            subtitle += (
+                f"【导演：{'/'.join([d.get('name') for d in self._ptgen.get('director')])}】"
+            )
+        if self._ptgen.get("writer"):
+            subtitle += (
+                f"【编剧：{'/'.join([w.get('name') for w in self._ptgen.get('writer')])}】"
+            )
+        if self._ptgen.get("cast"):
+            subtitle += (
+                f"【主演：{'/'.join([c.get('name') for c in self._ptgen.get('cast')[:3]])}】"
+            )
+        return subtitle
+
+    @property
     def mediaInfo(self):
+        return self._get_full_mediainfo()
+
+    @property
+    def mediaInfos(self):
+        return []
+
+    @property
+    def description(self):
+        return "{}\n\n[quote]{}{}[/quote]\n\n{}".format(
+            self._ptgen.get("format"),
+            self.mediaInfo,
+            "\n\n" + self.parsed_encoder_log if self.parsed_encoder_log else "",
+            "\n".join([f"[img]{url}[/img]" for url in self._screenshots]),
+        )
+
+    @property
+    def originalDescription(self):
+        return ''
+
+    @property
+    def doubanUrl(self):
+        if self._ptgen.get("site") == "douban":
+            return f"https://movie.douban.com/subject/{self._ptgen.get('sid')}"
+        return ""
+
+    @property
+    def doubanInfo(self):
+        return ''
+
+    @property
+    def imdbUrl(self):
+        return self._ptgen.get("imdb_link", "")
+
+    @property
+    def screenshots(self):
+        return self._screenshots
+
+    @property
+    def poster(self):
+        return self._ptgen.get("poster", "")
+
+    @property
+    def year(self):
+        return self._ptgen.get("year", "")
+
+    @property
+    def category(self):
+        if "演唱会" in self._ptgen.get("tags", []) and "音乐" in self._ptgen.get(
+            "genre", []
+        ):
+            return "concert"
+        imdb_genre = self._imdb.get("genre", [])
+        if "Documentary" in imdb_genre:
+            return "documentary"
+        imdb_type = self._imdb.get("@type", "")
+        if imdb_type == "Movie":
+            return "movie"
+        if imdb_type == "TVSeries":
+            return "tvPack"
+        return imdb_type
+
+    @property
+    def videoType(self):
+        if "webdl" in self.folder.lower() or "web-dl" in self.folder.lower():
+            return "web"
+        elif "remux" in self.folder.lower():
+            return "remux"
+        elif "hdtv" in self.folder.lower():
+            return "hdtv"
+        elif any(e in self.folder.lower() for e in ("x264", "x265")):
+            return "encode"
+        elif "bluray" in self.folder.lower() and not any(
+            e in self.folder.lower() for e in ("x264", "x265")
+        ):
+            return "bluray"
+        elif "uhd" in self.folder.lower():
+            return "uhdbluray"
+        for track in self._mediainfo.tracks:
+            if track.track_type == "Video":
+                if track.encoding_settings:
+                    return "encode"
+        return ""
+
+    @property
+    def format(self):
+        # TODO: Maybe read from mediainfo
+        return self._main_file.suffix
+
+    @property
+    def source(self):
+        return ""
+
+    @property
+    def videoCodec(self):
+        for track in self._mediainfo.tracks:
+            if track.track_type == "Video":
+                if track.encoded_library_name:
+                    return track.encoded_library_name
+                if track.commercial_name == "AVC":
+                    return "x264"
+        return ""
+
+    @property
+    def audioCodec(self):
+        codec_map = {
+            "AAC": "aac",
+            "Dolby Digital Plus": "dd+",
+            "Dolby Digital": "dd",
+            "DTS-HD Master Audio": "dtshdma",
+            "Dolby Digital Plus with Dolby Atmos": "atmos",
+            "Dolby TrueHD": "truehd",
+        }
+        for track in self._mediainfo.tracks:
+            if track.track_type == "Audio":
+                if track.format_info == "Audio Coding 3":
+                    return "ac3"
+                if track.format_info == "Free Lossless Audio Codec":
+                    return "flac"
+                if track.commercial_name in codec_map:
+                    return codec_map.get(track.commercial_name)
+                # TODO: other formats
+                # dts: "3",
+                # lpcm: "21",
+                # dtsx: "3",
+                # ape: "2",
+                # wav: "22",
+                # mp3: "4",
+                # m4a: "5",
+                # other: "7"
+        return ""
+
+    @property
+    def resolution(self):
+        for track in self._mediainfo.tracks:
+            if track.track_type == "Video":
+                if track.height <= 480:
+                    return "480p"
+                elif track.height <= 576:
+                    return "576p"
+                elif track.height <= 720:
+                    return "720p"
+                elif track.height <= 1080:
+                    if getattr(track, "scan_type__store_method") == "InterleavedFields":
+                        return "1080i"
+                    return "1080p"
+                elif track.height <= 2160:
+                    return "2160p"
+                elif track.height <= 4320:
+                    return "4320p"
+        return ""
+
+    @property
+    def area(self):
+        area_map = {
+            "中国大陆": "CN",
+            "中国香港": "HK",
+            "中国台湾": "TW",
+            "美国": "US",
+            "日本": "JP",
+            "韩国": "KR",
+            "印度": "IND",
+            "法国": "EU",
+            "意大利": "EU",
+            "德国": "EU",
+            "西班牙": "EU",
+            "葡萄牙": "EU",
+        }
+        regions = self._ptgen.get("region", [])
+        for area in area_map.keys():
+            if area in regions:
+                return area_map[area]
+        return ""
+
+    @property
+    def movieName(self):
+        if self._ptgen.get("site") == "imdb":
+            return self._ptgen.get("name", "")
+        if self._ptgen.get("site") == "douban":
+            return self._ptgen.get("aka", [""])[0]
+        return ""
+
+    @property
+    def movieAkaName(self):
+        return ""
+
+    @property
+    def size(self):
+        for track in self._mediainfo.tracks:
+            if track.track_type == "General":
+                return track.file_size
+        return ""
+
+    @property
+    def tags(self):
+        tags = {}
+        for track in self._mediainfo.tracks:
+            if track.track_type == "General":
+                if track.audio_language_list and "Chinese" in track.audio_language_list:
+                    tags["chinese_audio"] = True
+                if track.text_language_list and "Chinese" in track.text_language_list:
+                    tags["chinese_subtitle"] = True
+        # TODO: hdr, hdr10_plus, dolby_vision, diy, cantonese_audio, false,dts_x, dolby_atoms
+        return tags
+
+    @property
+    def otherTags(self):
+        return []
+
+    @property
+    def comparisons(self):
+        return []
+
+    @property
+    def torrentInfo(self):
+        return {
+            "title": self.title,
+            "subtitle": self.subtitle,
+            "description": self.description,
+            "originalDescription": self.originalDescription,
+            "doubanUrl": self.doubanUrl,
+            "doubanInfo": self.doubanInfo,
+            "imdbUrl": self.imdbUrl,
+            "mediaInfo": self.mediaInfo,
+            "mediaInfos": self.mediaInfos,
+            "screenshots": self.screenshots,
+            "poster": self.poster,
+            "year": self.year,
+            "category": self.category,
+            "videoType": self.videoType,
+            "format": self.format,
+            "source": self.source,
+            "videoCodec": self.videoCodec,
+            "audioCodec": self.audioCodec,
+            "resolution": self.resolution,
+            "area": self.area,
+            "movieAkaName": self.movieAkaName,
+            "movieName": self.movieName,
+            "size": self.size,
+            "tags": self.tags,
+            "otherTags": self.otherTags,
+            "comparisons": self.comparisons,
+            "isForbidden": False,
+            "sourceSiteType": "NexusPHP",
+        }
+
+    @abstractmethod
+    def upload(self):
         raise NotImplementedError()
