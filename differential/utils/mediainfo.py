ff5cd2487a7df54529c401bd83850696add52288
Lei Shi <me@leishi.io>
2022-03-15 13:40:11 +0100

    Add file size to full mediainfo

--- differential/utils/mediainfo.py
+++ differential/utils/mediainfo.py
@@ -56,6 +56,7 @@ def get_full_mediainfo(mediainfo: MediaInfo) -> str:
             "Complete name",
             "Format",
             "Format version",
+            "File Size",
             "Duration",
             "Overall bit rate",
             "Encoded date",
